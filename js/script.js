02a1b4cb8387ee2091d555e021748e551506d868
Wellington Thalyson Barreiros <WellingtonT@fuvs.br>
2019-06-05 09:39:29 -0300

    Finalizado site

--- js/script.js
+++ js/script.js
@@ -4,6 +4,20 @@ function start(){
 	var buttons = getButtons();
 	
 	addClickInButtons(buttons);
+
+	addEffectLinksMenu();
+}
+
+function addEffectLinksMenu(){
+	$('a[href^="#"]').on('click', function(e) {
+		e.preventDefault();
+		var id = $(this).attr('href'),
+				targetOffset = $(id).offset().top;
+				
+		$('html, body').animate({ 
+			scrollTop: targetOffset - 100
+		}, 500);
+	});
 }
 
 function getButtons(){
